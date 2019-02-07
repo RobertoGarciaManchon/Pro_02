6d320dbb5a7b97c857364b4234ec8a2ab0ad3051
RobertoGarciaManchon <robertogm3@gmail.com>
2019-02-07 19:08:39 +0100

    # WARNING: head commit changed in the meantime
    hola alandalus

---
+++ prueba_git_eclipse1/src/prueba_git_eclipse1.java
@@ -0,0 +1,9 @@
+
+public class prueba_git_eclipse1 {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("hola alandalus");
+	}
+
+}
