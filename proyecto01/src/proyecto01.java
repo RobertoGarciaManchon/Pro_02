c383ae1718484deadd07d3f56297272322c49eb0
RobertoGarciaManchon <robertogm3@gmail.com>
2019-02-07 14:13:57 +0100

    proyecto01 vacio

--- proyecto01/src/proyecto01.java
+++ proyecto01/src/proyecto01.java
@@ -1,9 +1,9 @@
 
-public class proyecto3 {
+public class proyecto01 {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
-//inico del proyecto. vacío
+
 	}
 
 }
