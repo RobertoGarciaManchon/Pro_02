e23bdf4497285ace79e4e933e3b03d6b98742d2b
RobertoGarciaManchon <robertogm3@gmail.com>
2019-02-07 22:55:32 +0100

    Inicio del proyecto

--- pro_02/src/pro_02.java
+++ pro_02/src/pro_02.java
@@ -1,11 +1,9 @@
 
-public class pro_01 {
+public class pro_02 {
 
 	public static void main(String[] args) {
 		// TODO Apéndice de método generado automáticamente
-//commit 1
-		//commit 2
-		//commit 3
+// versión inicial
 	}
 
 }
